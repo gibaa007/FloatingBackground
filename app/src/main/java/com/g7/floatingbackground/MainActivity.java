a5a92a8885f52adf98bd4e87a2d6a238943fc03d
Gibin <gibin@newagesmb.com>
2017-06-29 20:38:16 +0530

    Initial commit

---
+++ app/src/main/java/com/g7/floatingbackground/MainActivity.java
@@ -0,0 +1,39 @@
+package com.g7.floatingbackground;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GradientBackgroundPainter gradientBackgroundPainter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View backgroundImage = findViewById(R.id.root_view);
+        findViewById(R.id.tv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Did it", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final int[] drawables = new int[3];
+        drawables[0] = R.drawable.gradient_1;
+        drawables[1] = R.drawable.gradient_2;
+        drawables[2] = R.drawable.gradient_3;
+
+        gradientBackgroundPainter = new GradientBackgroundPainter(backgroundImage, drawables);
+        gradientBackgroundPainter.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        gradientBackgroundPainter.stop();
+    }
+}
